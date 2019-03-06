8fd359c8423cc3a7f82101e094e11cdf15b6eac6
amit <amit.kadivar1@gmail.com>
2019-03-06 11:41:04 +0530

    change in seetiong file

--- orders/urls.py
+++ orders/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('create/',views.order_create,name='order_create'),
     path('admin/order/<int:order_id>/',views.admin_order_detail,name="admin_order_detail"),
     # path('admin/order/<int:order_id>/pdf/',views.admin_order_pdf,name='admin_order_pdf'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
