cb65b3da49bb401432cbaf9ef417f7901abf30ea
amit <amit.kadivar1@gmail.com>
2019-03-06 11:48:45 +0530

    change in seetiong file

--- myshop/urls.py
+++ myshop/urls.py
@@ -25,7 +25,5 @@ urlpatterns = [
     path('payment/', include('payment.urls', namespace='payment')),
     path('',include('shop.urls',namespace='shop')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
