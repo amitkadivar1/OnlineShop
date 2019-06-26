4b6d3369fedd1b1d6e0ca5e9f7f468f395103222
amit <amit.kadivar1@gmail.com>
2019-06-26 23:51:36 +0530

    add dropbox

--- shop/views.py
+++ shop/views.py
@@ -12,7 +12,6 @@ def product_list(request, category_slug=None):
         products = products.filter(category=category)
     return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
 
-
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
