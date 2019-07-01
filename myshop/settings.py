7be22a39aa307cd649653ae1e4a31f95c6a8d353
amit <amit.kadivar1@gmail.com>
2019-07-02 01:51:11 +0530

    add payment recept

--- myshop/settings.py
+++ myshop/settings.py
@@ -186,5 +186,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'amit.kadivar3@gmail.com'
+EMAIL_HOST_PASSWORD = 'Shreeji@Amit@Ce@1234'
